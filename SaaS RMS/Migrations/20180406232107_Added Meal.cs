fb5e8815d811f2e38f8d6871514fd9d6774d2b84
meckblvck <meckydrix@gmail.com>
2018-04-07 00:21:59 +0100

    Migrated Meal back to the database

---
+++ SaaS RMS/Migrations/20180406232107_Added Meal.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SaaSRMS.Migrations
+{
+    public partial class AddedMeal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Restaurants_States_StateId",
+                table: "Restaurants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Restaurants_StateId",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "StateId",
+                table: "Restaurants");
+
+            migrationBuilder.CreateTable(
+                name: "Meals",
+                columns: table => new
+                {
+                    MealId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    RestaurantId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meals", x => x.MealId);
+                    table.ForeignKey(
+                        name: "FK_Meals_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurants",
+                        principalColumn: "RestaurantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dish",
+                columns: table => new
+                {
+                    DishId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<double>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: false),
+                    MealId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dish", x => x.DishId);
+                    table.ForeignKey(
+                        name: "FK_Dish_Meals_MealId",
+                        column: x => x.MealId,
+                        principalTable: "Meals",
+                        principalColumn: "MealId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dish_MealId",
+                table: "Dish",
+                column: "MealId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meals_RestaurantId",
+                table: "Meals",
+                column: "RestaurantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dish");
+
+            migrationBuilder.DropTable(
+                name: "Meals");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StateId",
+                table: "Restaurants",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_StateId",
+                table: "Restaurants",
+                column: "StateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Restaurants_States_StateId",
+                table: "Restaurants",
+                column: "StateId",
+                principalTable: "States",
+                principalColumn: "StateId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
