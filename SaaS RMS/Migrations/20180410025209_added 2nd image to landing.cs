ed11d00471f5c79d70efde59bfc690cbc22f0f7b
meckblvck <meckydrix@gmail.com>
2018-04-10 03:52:45 +0100

    Added a second image

---
+++ SaaS RMS/Migrations/20180410025209_added 2nd image to landing.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SaaSRMS.Migrations
+{
+    public partial class added2ndimagetolanding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SecondImage",
+                table: "LandingInfo",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecondImage",
+                table: "LandingInfo");
+        }
+    }
+}
