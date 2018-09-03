82d97dd98346f11e687898ee2455aa06ff171bd2
Meck Tinum <meck.tinum@hotmail.com>
2018-09-03 01:06:48 +0100

    Added Hashing

---
+++ SaaS RMS/Models/Encryption/Hashing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaaS_RMS.Models.Encryption
+{
+    public class Hashing
+    {
+        private string GetRandomSalt()
+        {
+            return BCrypt.GenerateSalt(12);
+        }
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.HashPassword(password, GetRandomSalt());
+        }
+
+        public bool ValidatePassword(string password, string correctHash)
+        {
+            return BCrypt.Verify(password, correctHash);
+        }
+    }
+}
